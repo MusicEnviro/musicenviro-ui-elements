966102e06e18a97741afec426cff8c4325ecd35e
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-04-03 22:43:56 -0400

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { PianoKeyboard, IPianoKeyboardProps} from "./components/PianoKeyboard/PianoKeyboard";
 export { GuitarChordDiagram, IGuitarChordDiagramProps, IFingeredNote } from "./components/GuitarChordDiagram/GuitarChordDiagram";
 export * from "./components/ButtonGrid";
+export * from "./components/SingleNoteLane/SingleNoteLane";
+export * from "./components/MultiNoteLanes/MultiNoteLanes";
