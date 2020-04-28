1a7e8dc4d9ac78379da4af4ed7cd3154df8a1915
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-04-28 14:09:26 -0400

    update diatonic piano roll example

--- src/simple-client/examples/DiatonicPianoRollExample.tsx
+++ src/simple-client/examples/DiatonicPianoRollExample.tsx
@@ -1,8 +1,29 @@
-import * as React from 'react'
-import { DiatonicPianoRoll } from '../../components/DiatonicPianoRoll/DiatonicPianoRoll';
+import * as React from 'react';
+import {
+	DiatonicPianoRoll,
+	ILaneData,
+	modifyLaneCell,
+	makeDefaultLanes,
+} from '../../components/DiatonicPianoRoll/DiatonicPianoRoll';
 
 export function DiatonicPianoRollExample() {
-	return <DiatonicPianoRoll 
-	height={250} width={900}
-	onCellChange={console.log} />;
+	const stepRange = {
+		min: 0,
+		max: 14
+	}
+	
+	const [lanes, setLanes] = React.useState<ILaneData[]>(makeDefaultLanes(stepRange));
+
+	return (
+		<DiatonicPianoRoll
+			height={250}
+			width={900}
+			stepRange={stepRange}
+			initialLanes={lanes}
+			onCellChange={(laneIndex, cellIndex, active) => {
+				console.log(laneIndex, cellIndex, active);
+				setLanes(modifyLaneCell(lanes, laneIndex, cellIndex, active));
+			}}
+		/>
+	);
 }
