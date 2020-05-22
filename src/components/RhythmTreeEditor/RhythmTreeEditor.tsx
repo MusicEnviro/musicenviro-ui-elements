908675ff22c62c9efc74d8b80514d51656b6ceb6
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-05-21 22:05:21 -0400

    use html divs instead of canvas

--- src/components/RhythmTreeEditor/RhythmTreeEditor.tsx
+++ src/components/RhythmTreeEditor/RhythmTreeEditor.tsx
@@ -21,6 +21,8 @@ const defaultProps: IRhythmTreeEditorProps = {
 export const RhythmTreeEditor: FunctionComponent<IRhythmTreeEditorProps> = props => {
 	const canvasRef = useRef<HTMLCanvasElement>();
 	const [tree, setTree] = useState<IRhythmTree>();
+	const [blocks, setBlocks] = useState<IBlock[]>([]);
+	const [selectedId, setSelectedId] = useState<number>();
 
 	useEffect(() => {
 		// use different representation with an extra node above
@@ -28,26 +30,70 @@ export const RhythmTreeEditor: FunctionComponent<IRhythmTreeEditorProps> = props
 	}, [props]);
 
 	useEffect(() => {
-		redraw();
+		if (!tree) return;
+		setBlocks(treeToBlocks(tree, 0));
 	}, [tree]);
 
+	useEffect(() => {
+		// redraw();
+	}, [blocks]);
+
+	// canvas drawing, maybe deprecate
 	function redraw() {
-		if (!tree) return
+		if (!tree) return;
 		const ctx = canvasRef.current.getContext('2d');
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-		drawBlocksInCanvas(ctx, treeToBlocks(tree, 0));
+		drawBlocksInCanvas(ctx, blocks, selectedId);
 	}
 
 	return (
-		<div className="volume-knob" style={{ width: props.width, height: props.height }}>
-			<canvas
+		<div
+			className="rhtyhm-tree-editor"
+			style={{ position: 'relative', width: props.width, height: props.height }}
+		>
+			{...blocks.map(block => (
+				<div
+					style={{
+						position: 'absolute',
+
+						left: toPercent(block.rect.left),
+						top: toPercent(block.rect.top),
+						width: toPercent(block.rect.right - block.rect.left),
+						height: toPercent(block.rect.bottom - block.rect.top),
+						lineHeight: props.height * (block.rect.bottom - block.rect.top) + 'px',
+						backgroundColor: getColor(block, block.id === selectedId),
+
+						border: 'solid 2px white',
+						textAlign: 'center',
+						color: 'white',
+					}}
+
+					onClick={() => setSelectedId(block.id)}
+				>
+					<p
+						style={{
+							lineHeight: 1.5,
+							display: 'inline-block',
+							verticalAlign: 'middle',
+						}}
+					>
+						{block.unitLength.toString()}
+					</p>
+				</div>
+			))}
+
+			{/* <canvas
 				ref={canvasRef}
 				width={props.width}
 				height={props.height}
 				style={{ backgroundColor: props.backgroundColor, borderRadius: 3 }}
-			></canvas>
+			></canvas> */}
 		</div>
 	);
+
+	function toPercent(n: number) {
+		return (n * 100).toString() + '%';
+	}
 };
 RhythmTreeEditor.defaultProps = defaultProps;
 
@@ -58,7 +104,7 @@ interface IBlock {
 	id?: number;
 }
 
-function drawBlocksInCanvas(ctx: CanvasRenderingContext2D, blocks: IBlock[]) {
+function drawBlocksInCanvas(ctx: CanvasRenderingContext2D, blocks: IBlock[], selectedId) {
 	const padding = 2;
 
 	ctx.font = '12px Arial';
@@ -72,7 +118,7 @@ function drawBlocksInCanvas(ctx: CanvasRenderingContext2D, blocks: IBlock[]) {
 			bottom: block.rect.bottom * ctx.canvas.height,
 		};
 
-		ctx.fillStyle = `rgb(${255 - block.depth * 40}, 0, 0)`;
+		ctx.fillStyle = getColor(block, block.id === selectedId);
 		ctx.fillRect(
 			rect.left + padding,
 			rect.top + padding,
@@ -87,6 +133,11 @@ function drawBlocksInCanvas(ctx: CanvasRenderingContext2D, blocks: IBlock[]) {
 			(rect.bottom + rect.top) / 2,
 		);
 	});
+}
+
+function getColor(block: IBlock, selected: boolean): string {
+	const byte = selected ? 255 - block.depth * 40 : 175 - block.depth * 20;
+	return selected ? `rgb(${byte}, 0, 0)` : `rgb(${byte}, ${byte}, ${byte})`;
 }
 
 function treeToBlocks(tree: IRhythmTree, depth: number): IBlock[] {
@@ -107,9 +158,9 @@ function treeToBlocks(tree: IRhythmTree, depth: number): IBlock[] {
 				},
 				depth,
 				unitLength: nodeUnitLength(node),
-				id: typeof node === 'number' ? null : node.id 
+				id: typeof node === 'number' ? null : node.id,
 			});
-		} else {	
+		} else {
 			result.push(
 				{
 					rect: {
@@ -120,7 +171,7 @@ function treeToBlocks(tree: IRhythmTree, depth: number): IBlock[] {
 					},
 					depth,
 					unitLength: node.units,
-					id: node.id
+					id: node.id,
 				},
 				...fitBlocksToRect(treeToBlocks(node.subtree, depth + 1), {
 					left: edges[i],
