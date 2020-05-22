30f6ef4ec18036f080f17d51fa696e1ed53fb301
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-05-22 09:19:22 -0400

    prevent default arrow action

--- src/simple-client/examples/RhythmTreeEditorExample.tsx
+++ src/simple-client/examples/RhythmTreeEditorExample.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { RhythmTreeEditor } from '../../components/RhythmTreeEditor/RhythmTreeEditor';
 import { tree44 } from '../../musical-data/trees';
 
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 const exampleTree1 = {
 	nodes: [
 		1,
